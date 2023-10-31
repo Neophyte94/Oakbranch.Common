9df76cfb8b5f606a76425fe99f697cf1242601e6
Neophyte94 <guerrero940409@gmail.com>
2023-11-01 01:17:40 +0200

    Added XML comments.

--- Oakbranch.Common/Logging/ILogger.cs
+++ Oakbranch.Common/Logging/ILogger.cs
@@ -2,11 +2,28 @@
 
 namespace Oakbranch.Common.Logging
 {
+    /// <summary>
+    /// Represents an abstraction for performing logging.
+    /// </summary>
     public interface ILogger
     {
+        /// <summary>
+        /// Gets the current log level.
+        /// </summary>
         LogLevel Level { get; }
 
+        /// <summary>
+        /// Determines whether the given log level is enabled.
+        /// </summary>
+        /// <param name="level">The log level to check.</param>
+        /// <returns><see langword="true"/> if the specified log level is enabled; otherwise, <see langword="false"/>.</returns>
         bool IsLevelEnabled(LogLevel level);
+        /// <summary>
+        /// Writes a log message at the specified log level.
+        /// </summary>
+        /// <param name="level">The log level of the message.</param>
+        /// <param name="context">The context or category of the message.</param>
+        /// <param name="message">The message to be logged.</param>
         void Log(LogLevel level, string context, string message);
     }
 }
--- Oakbranch.Common/Logging/LogLevel.cs
+++ Oakbranch.Common/Logging/LogLevel.cs
@@ -7,9 +7,24 @@ namespace Oakbranch.Common.Logging
     /// </summary>
     public enum LogLevel
     {
-        Debug,
-        Info,
-        Warning,
-        Error
+        /// <summary>
+        /// Logs that are used for interactive investigation during development.
+        /// <para>These logs should primarily contain information useful for debugging and have no long-term value.</para>
+        /// </summary>
+        Debug = 0,
+        /// <summary>
+        /// Logs that track the general flow of the application.
+        /// <para>These logs should have long-term value.</para>
+        /// </summary>
+        Info = 1,
+        /// <summary>
+        /// Logs that highlight abnormal or unexpected events in the application flow
+        /// not causing a termination of operations involved.
+        /// </summary>
+        Warning = 2,
+        /// <summary>
+        /// Logs that highlight failures causing a termination of the current flow of the application.
+        /// </summary>
+        Error = 3
     }
 }
